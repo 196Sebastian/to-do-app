798d61e1b2d0f3cd7503408c35d81fdf223107e2
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-17 12:52:29 -0500

    Design UpdateFragment Layout

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -21,6 +22,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
         view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
         return view
     }
