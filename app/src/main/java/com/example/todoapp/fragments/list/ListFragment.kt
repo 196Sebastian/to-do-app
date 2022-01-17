f53db53e600a209c9cf4fbb8a997eb8da9b53929
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-17 13:05:24 -0500

    Created menu for ListFragment

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
  package com.example.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
@@ -27,6 +25,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
         view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
         return view
     }
+
+     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+         inflater.inflate(R.menu.list_fragment_menu, menu)
+     }
 }
